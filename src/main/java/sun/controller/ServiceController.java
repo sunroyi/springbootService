f83dd9ee0bae7585c19c60f929b59934490fb5c1
Sun.wenli <sunroyi@hotmail.co.jp>
2019-09-09 11:13:23 +0800

    first

---
+++ src/main/java/sun/controller/ServiceController.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package sun.controller;
+
+import common.entity.RestfulResult;
+import common.utils.CommUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sun.entity.ServiceInfo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping(value = "service")
+public class ServiceController {
+
+    @RequestMapping(value = "hello")
+    public void login(HttpServletRequest request, HttpServletResponse response,
+                      @RequestBody ServiceInfo serviceInfo) {
+
+        RestfulResult restfulResult = new RestfulResult();
+
+        try {
+            restfulResult.setData("Welcome " + serviceInfo.getName());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        CommUtils.printDataJason(response, restfulResult);
+    }
+}

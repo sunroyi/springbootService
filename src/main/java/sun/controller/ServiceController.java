42ba2fc5e6680d8bfab66f6fb671806a3f9630c4
Sun.wenli <sunroyi@hotmail.co.jp>
2019-09-12 09:35:14 +0800

    2

--- src/main/java/sun/controller/ServiceController.java
+++ src/main/java/sun/controller/ServiceController.java
@@ -24,7 +24,7 @@ public class ServiceController {
         RestfulResult restfulResult = new RestfulResult();
 
         try {
-            restfulResult.setData("Welcome " + serviceInfo.getName() + "!!!");
+            restfulResult.setData("Welcome " + serviceInfo.getName() + "!+");
 
         } catch (Exception e) {
             e.printStackTrace();
